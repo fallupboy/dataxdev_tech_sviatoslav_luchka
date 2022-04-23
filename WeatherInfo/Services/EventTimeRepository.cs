09a1cedc62bcfcea159db3a4a9818c98a0f42813
fallupboy <svity21@gmail.com>
2022-04-22 20:01:22 -0500

    Modify EventTime service

--- WeatherInfo/Services/EventTimeRepository.cs
+++ WeatherInfo/Services/EventTimeRepository.cs
@@ -24,19 +24,19 @@ namespace WeatherInfo.Services
                 Event sunInfo = new Event();
                 using (var client = new HttpClient())
                 {
-                    //Passing service base url
+                    // Passing service base url
                     client.BaseAddress = new Uri(Baseurl);
                     client.DefaultRequestHeaders.Clear();
-                    //Define request data format
+                    // Define request data format
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                    // Sending request to find web api REST service resource using HttpClient
                     HttpResponseMessage Res = await client.GetAsync($"json?lat={latitude}&lng={longitude}&date=today");
-                    //Checking the response is successful or not which is sent using HttpClient
+                    // Checking the response is successful or not which is sent using HttpClient
                     if (Res.IsSuccessStatusCode)
                     {
-                        //Storing the response details recieved from web api
+                        // Storing the response details recieved from web api
                         var SunResponse = Res.Content.ReadAsStringAsync().Result;
-                        //Deserializing the response recieved from web api and storing into the Employee list
+                        // Deserializing the response recieved from web api and storing into the Event data
                         sunInfo = JsonConvert.DeserializeObject<Event>(SunResponse);
                         return sunInfo;
                     }
